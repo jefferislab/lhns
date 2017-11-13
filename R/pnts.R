d58c57a1b5a51a57b64ae8b6318035b9c40d0419
Gregory Jefferis <jefferis@gmail.com>
2017-11-13 16:20:00 +0000

    import rgl
    Former-commit-id: eea2f060956cc945626f5347caa14b8f1a1174c3

--- R/pnts.R
+++ R/pnts.R
@@ -6,18 +6,19 @@
 #'   (default=\code{TRUE})
 #' @seealso \code{\link[nat.flybrains]{FCWB}}
 #' @export
+#' @importFrom rgl par3d plot3d text3d scaleMatrix
 plot_pnts <- function(open=TRUE){
   if(open){
     nat::nopen3d()
-    rgl::par3d(userMatrix=rgl::scaleMatrix(1,-1,-1))
+    par3d(userMatrix=scaleMatrix(1,-1,-1))
   }
-  rgl::plot3d(lhns::primary.neurite.tracts,soma=T,lwd=5, skipRedraw = TRUE)
+  plot3d(lhns::primary.neurite.tracts,soma=T,lwd=5, skipRedraw = TRUE)
   pxyz= amount = t(sapply(lhns::primary.neurite.tracts, function(x) nat::xyzmatrix(x)[nat::rootpoints(x),]))
   rownames(pxyz) = gsub(pattern = "LH|lh","",names(lhns::primary.neurite.tracts))
   shift <- matrix(c(-3, 3, 0), nrow(pxyz), 3, byrow = TRUE)
-  rgl::text3d(pxyz + shift,texts =rownames(pxyz))
+  text3d(pxyz + shift,texts =rownames(pxyz))
   rownames(amount) = sapply(lhns::primary.neurite.tracts[,"pnt"], function(x) length(lhns::most.lhns[lhns::most.lhns[,"pnt"]%in%x]))
   shift2 <- matrix(c(-3, 10, 0), nrow(amount), 3, byrow = TRUE)
-  rgl::text3d(amount + shift2,texts =rownames(amount),col="red",cex=0.75)
-  rgl::plot3d(nat.flybrains::FCWB)
+  text3d(amount + shift2,texts =rownames(amount),col="red",cex=0.75)
+  plot3d(nat.flybrains::FCWB)
 }
