1691f0f74f42eaa8cce0b3ffff163eb30a70366a
Gregory Jefferis <jefferis@gmail.com>
2017-11-13 13:41:20 +0000

    remove unused package docs
    * import nat.flybrains to squelch check error
    
    
    Former-commit-id: 4a8deeb6f2419ab434584ca26f0bfb61281f518e

---
+++ R/lhns-package.r
@@ -0,0 +1,9 @@
+#' Skeleton data for Drosophila melanogaster lateral horn (associated) neurons
+#'
+#' The key data objects provided by this package is the \code{\link{most.lhns}}
+#' containing neurons
+#'
+#' @name lhns
+#' @docType package
+#' @import nat.flybains
+NULL
