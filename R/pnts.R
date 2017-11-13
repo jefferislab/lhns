3d64269060e22beedb7da6dc44e1a19ebe9fa961
Gregory Jefferis <jefferis@gmail.com>
2017-11-13 11:46:48 +0000

    improvements to plot_pnts
    * speed up
    * make nopen optional
    * frontal view
    
    
    Former-commit-id: c8e0fe5673ad65e673faf02382c0a28d29099b9a

--- R/pnts.R
+++ R/pnts.R
@@ -1,12 +1,17 @@
 #' Plot adult Lateral Horn primary neurite tracts
 #'
-#' @description Plots the primary neurite tracts from Frechter et al. 2017 in FCWB space.
-#'
+#' @description Plots the primary neurite tracts from Frechter et al. 2017 in
+#'   \code{\link[nat.flybrains]{FCWB}} space.
+#' @param open Whether to open a new RGL window for plotting
+#'   (default=\code{TRUE})
+#' @seealso \code{\link[nat.flybrains]{FCWB}}
 #' @export
-plot_pnts <- function(){
-  nat::nopen3d()
-  rgl::plot3d(nat.flybrains::FCWB)
-  rgl::plot3d(lhns::primary.neurite.tracts,soma=T,lwd=5)
+plot_pnts <- function(open=TRUE){
+  if(open){
+    nat::nopen3d()
+    rgl::par3d(userMatrix=rgl::scaleMatrix(1,-1,-1))
+  }
+  rgl::plot3d(lhns::primary.neurite.tracts,soma=T,lwd=5, skipRedraw = TRUE)
   pxyz= amount = t(sapply(lhns::primary.neurite.tracts, function(x) nat::xyzmatrix(x)[nat::rootpoints(x),]))
   rownames(pxyz) = gsub(pattern = "LH|lh","",names(lhns::primary.neurite.tracts))
   shift <- matrix(c(-3, 3, 0), nrow(pxyz), 3, byrow = TRUE)
@@ -14,4 +19,5 @@ plot_pnts <- function(){
   rownames(amount) = sapply(lhns::primary.neurite.tracts[,"pnt"], function(x) length(lhns::most.lhns[lhns::most.lhns[,"pnt"]%in%x]))
   shift2 <- matrix(c(-3, 10, 0), nrow(amount), 3, byrow = TRUE)
   rgl::text3d(amount + shift2,texts =rownames(amount),col="red",cex=0.75)
+  rgl::plot3d(nat.flybrains::FCWB)
 }
