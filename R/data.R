03ab82f3938fa5cbd4ba910d211f64bef5ba5a35
alexanderbates <alexander.shakeel.bates@gmail.com>
2017-11-13 02:23:26 +0000

    Updated R object descriptions
    * Needs to be improved, i.e. citations
    
    
    Former-commit-id: 64d7a58241e0520299e325c4df2caaaac213cdd1

--- R/data.R
+++ R/data.R
@@ -12,14 +12,16 @@
 #' @source \url{http://www.flycircuit.tw/}
 "most.lhns.dps"
 
-#' Lateral horn primary neurite tracts
-#'
-#' The known primary neurite tracts that lateral horn neurons can occupy
-#'
-"primary.neurite.tracts"
 
 #' Lateral horn primary neurite tracts as a dotprops object
 #'
 #' A dotprops dataset containing the known primary neurite tracts that lateral horn neurons can occupy
 #'
 "most.lhns.pnts.dps"
+
+
+#' Projection neuron response data
+#'
+#' Ca2+ responses in the dendrite of Drosophila, uniglomerular GH146 positive projection neurons from Badel et al. 2017.
+#'
+"Badel.PN.responses"
