cd4dbf677a5f21ad315032caf2038a95341bd892
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-03-14 11:46:12 +0000

    Be able to plot primary neurite tracts in different brain spaces
    * Plot pnts using plot_pnts() in spaces other than FCWB using ‘brain’
    argument
    
    
    Former-commit-id: 518e78f367742b26f52b14920ac6065ba6167789 [formerly 9d17b2ae50cf85bd90b71ac5e65d25a443ef5aa0] [formerly 9d17b2ae50cf85bd90b71ac5e65d25a443ef5aa0 [formerly 9c06702abb3ecd7274190434c080e310f708ce88]] [formerly 1287a1a3bb414f0996a76c97b939fe9312f80ab6] [formerly bd899b7edf499d797cbabea36fb9b9b261c2ed0d] [formerly bd899b7edf499d797cbabea36fb9b9b261c2ed0d [formerly bd899b7edf499d797cbabea36fb9b9b261c2ed0d [formerly bd899b7edf499d797cbabea36fb9b9b261c2ed0d [formerly bd899b7edf499d797cbabea36fb9b9b261c2ed0d [formerly d88af205acdb39b012ba0e396649029540012722]]]]]
    Former-commit-id: f1730a3d1fe944869c28c7975d6b2c19aa68afa2
    Former-commit-id: 961d54f92eff9bef59aa46db337e26177ba547db [formerly 91b9d6ee603d4c30b6ce85b831010cef32979432] [formerly e60db480072f264fca645d13ccdc141080aa2f10 [formerly a2be238ab6f5ccd8f1286ea61cfb52f1907e65c1]] [formerly e60db480072f264fca645d13ccdc141080aa2f10 [formerly a2be238ab6f5ccd8f1286ea61cfb52f1907e65c1] [formerly b5f0362a4544a0b7e9fdeccaa037f5cc05bb76bb]]
    Former-commit-id: 97e796f955af71aedea696f04bfde78d0d5384b6 [formerly ac1f238677d92dd22282d9e5538ab5cb41e4fa98] [formerly c913186073bdd7f149a184a6ff02547c27058b43 [formerly e54b0765129ec66652229e199c5adb778860e9e3]]
    Former-commit-id: ef50695e0c0c7dc09f483033caac2bd7cacfce7e [formerly 0bd229c0867e8cb556036be200669549c79b0272]
    Former-commit-id: d3e04a232feee3c82a3b024f470c3018011b72d1

--- R/pnts.R
+++ R/pnts.R
@@ -2,17 +2,23 @@
 #'
 #' @description Plots the primary neurite tracts from Frechter et al. 2017 in
 #'   \code{\link[nat.flybrains]{FCWB}} space.
-#' @param open Whether to open a new RGL window for plotting
+#' @param open whether to open a new RGL window for plotting
+#' @param brain the brainspace in which to plot the primary neurite tracts. Defaults to FCWB
 #'   (default=\code{TRUE})
 #' @seealso \code{\link[nat.flybrains]{FCWB}}
 #' @export
 #' @importFrom rgl par3d plot3d text3d scaleMatrix
-plot_pnts <- function(open=TRUE){
+plot_pnts <- function(open=TRUE, brain = nat.flybrains::FCWB){
   if(open){
     nat::nopen3d()
     par3d(userMatrix=scaleMatrix(1,-1,-1))
   }
-  plot3d(lhns::primary.neurite.tracts,soma=T,lwd=5, skipRedraw = TRUE)
+  if(brain!=nat.flybrains::FCWB){
+    pnts = nat.templatebrains::xform_brain(lhns::primary.neurite.tracts,sample=nat.flybrains::FCWB,reference = brain)
+  } else{
+    pnts = lhns::primary.neurite.tracts
+  }
+  plot3d(pnts,soma=T,lwd=5, skipRedraw = TRUE)
   pxyz= amount = t(sapply(lhns::primary.neurite.tracts, function(x) nat::xyzmatrix(x)[nat::rootpoints(x),]))
   rownames(pxyz) = gsub(pattern = "LH|lh","",names(lhns::primary.neurite.tracts))
   shift <- matrix(c(-3, 3, 0), nrow(pxyz), 3, byrow = TRUE)
