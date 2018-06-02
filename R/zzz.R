0f4c601e5b12b28e9ed0f209f6c9be881954d417
Gregory Jefferis <jefferis@gmail.com>
2018-06-02 13:25:27 +0100

    find_extdata needs to know name of the package to search
    * this is essential if you are looking inside the installed package rather than the current checkout

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,9 @@ lh.splits.dps <- structure(as.list(1:209), class=c('neuronlist', 'list'))
 
 .onLoad <- function(libname, pkgname) {
   # message("Building lh.splits")
-  base::delayedAssign('lhon.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhon.splits.dps.rds'))))
-  base::delayedAssign('lhln.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhln.splits.dps.rds'))))
-  base::delayedAssign('lhin.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhin.splits.dps.rds'))))
+  base::delayedAssign('lhon.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhon.splits.dps.rds', package='lhns'))))
+  base::delayedAssign('lhln.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhln.splits.dps.rds', package='lhns'))))
+  base::delayedAssign('lhin.splits.dps', nat::as.neuronlist(nat::read.neuronlistfh(nat.utils::find_extdata('lhin.splits.dps.rds', package='lhns'))))
   base::delayedAssign('lh.splits.dps', c(lhln.splits.dps,lhin.splits.dps,lhon.splits.dps))
   invisible()
 }
