e1de29df7a88fb3520e3ffd8660d81c9e7cf98e9
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-07-31 20:25:03 +0100

    Fixed critical error in odour naming for lhn_odour_responses
    * Columns were incorrectly named

--- data-raw/processEPhys.R
+++ data-raw/processEPhys.R
@@ -16,8 +16,20 @@ dye.fills = dye.fills[cells%in%names(physplitdata::smSpikes)] # Final list of ne
 
 # Get the E-Phys data
 ephys.lhns = physplit.analysis::create_raw_summary_array(x = physplitdata::smSpikes, cells = cells)
+
+# Get odours
 odours = colnames(ephys.lhns)
-ephys.lhns.m = physplit.analysis::createSummarySpikesMat(ephys.lhns,NALimit=3,numSamplePoints=7)
+odours.long = c("Mineral Oil", "trans-2-Hexenal", "trans-2-Hexenal", "Propyl acetate", "Isoamyl acetate", "Ethyl 3-hydroxybutyrate", "Nonanal",
+                "11-cis Vaccenyl Acetate", "Methyl salicylate", "Hexyl acetate", "Phenethyl alcohol", "Acetoin acetate", "Ethyl hexanoate", "2-Phenethyl acetate",
+                "5 odor mix", "Benzaldehyde", "β-Citronellol", "1-Hexanol", "Farnesol", "Water Blank", "Cadaverine",
+                "Spermine", "Acetoin", "Methyl acetate", "Methyl acetate", "Propionic acid", "Butyric acid", "Ammonia",
+                "Pyridine", "Pyridine", "HCl", "Phenylacetic acid", "Vinegar", "Geosmin", "Vineger and Geosmin mix",
+                "Phenethylamine", "Blank1", "Blank2", "FlyFM", "Ethylamine", "MtAmn", "Putrescine",
+                "Linalool", "2,3-Butanedione", "Spermidine", "CO2")
+names(odours.long) = odours
+
+# Get matrix
+ephys.lhns.m = physplit.analysis::createSummarySpikesMat(ephys.lhns,NALimit=10,numSamplePoints=7)
 choose.just.max1.cols = as.logical(rep(c(0,0,1,0,0,0,0),ncol(ephys.lhns.m)/7))
 ephys.lhns.m = ephys.lhns.m[,choose.just.max1.cols]
 colnames(ephys.lhns.m) = odours[1:ncol(ephys.lhns.m)]
@@ -27,12 +39,7 @@ ephys.lhns.m = ephys.lhns.m[order(rownames(ephys.lhns.m)),order(colnames(ephys.l
 
 # Select certain odours to show
 ephys.lhns.m = ephys.lhns.m[,!colnames(ephys.lhns.m)%in%c("ClrBL","ClrB2","FlyFM")]
-colnames(ephys.lhns.m) = c( "Mineral Oil", "trans-2-Hexenal", "Geranyl acetate", "Propyl acetate", "Isoamyl acetate", "Ethyl 3-hydroxybutyrate", "Nonanal",
-                            "11-cis vaccenyl acetate- CVA", "Methyl salicylate", "Hexyl acetate", "Phenethyl alcohol", "Acetoin acetate", "Ethyl hexanoate", "2-Phenethyl acetate", "5 odour mix", "Benzaldehyde", "b-citronellol", "1-Hexanol", "Farnesol", "Water blank", "Cadaverine",
-                            "Spermine", "Acetoin", "Methyl acetate", "Acetic acid", "Propionic Acid", "Butyric acid", "Ammonia",
-                            "Pyridine", "Phenylacetaldehyde", "Hydrochloric acid", "Phenylacetic acid", "Vinegar", "Geosmin",  "Vineger and Geosmin mix",
-                            "Phenylethylamine")
-
+colnames(ephys.lhns.m) = odours.long[colnames(ephys.lhns.m)]
 
 ########
 # Save #
