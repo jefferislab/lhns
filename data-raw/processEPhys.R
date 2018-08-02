721e250fa1f0ed9544257ea7bf9e74e84ebc46ae
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-08-02 23:37:42 +0100

    Include columns with NAs in odour response matrix
    * Also, PNs added

--- data-raw/processEPhys.R
+++ data-raw/processEPhys.R
@@ -15,36 +15,51 @@ cells = cells[cells%in%names(physplitdata::smSpikes)]
 dye.fills = dye.fills[cells%in%names(physplitdata::smSpikes)] # Final list of neurons for which there is e-phy data
 
 # Get the E-Phys data
-ephys.lhns = physplit.analysis::create_raw_summary_array(x = physplitdata::smSpikes, cells = cells)
+physdb = subset(PhySplitDB, !grepl("Old Odor Concentration",comments.IV) & Group %in%c("O", "PN", "L") & cell %in% names(Spikes))
+pns = subset(physdb, Group%in%c("PN"))[,"cell"]
+gloms = paste0(subset(physdb, Group%in%c("PN"))[,"Anatomy.type"]," PN")
+names(gloms) = pns
+cells = c(cells,pns)
+
+# Get ephys data
+ephys.lh = physplit.analysis::create_raw_summary_array(x = physplitdata::smSpikes, cells = cells)
 
 # Get odours
-odours = colnames(ephys.lhns)
+odours = colnames(ephys.lh)
 odours.long = c("Mineral Oil", "trans-2-Hexenal", "Geranyl acetate", "Propyl acetate", "Isoamyl acetate", "Ethyl 3-hydroxybutyrate", "Nonanal",
                 "11-cis Vaccenyl Acetate", "Methyl salicylate", "Hexyl acetate", "Phenethyl alcohol", "Acetoin acetate", "Ethyl hexanoate", "2-Phenethyl acetate",
                 "5 odor mix", "Benzaldehyde", "β-Citronellol", "1-Hexanol", "Farnesol", "Water Blank", "Cadaverine",
                 "Spermine", "Acetoin", "Methyl acetate", "Acetic acid", "Propionic acid", "Butyric acid", "Ammonia",
                 "Pyridine", "Phenylacetaldehyde", "HCl", "Phenylacetic acid", "Vinegar", "Geosmin", "Vinegar and Geosmin mix",
                 "Phenethylamine", "Blank1", "Blank2", "FlyFM", "Ethylamine", "MtAmn", "Putrescine",
-                "Linalool", "2,3-Butanedione", "Spermidine", "CO2")
+                "Linalool", "2,3-Butanedione", "Spermidine", "CO2", "Mineral Oil","Valencene", "Methyl laurate",
+                "Methyl palmitate", "Methyl myristate", "4-Ethylguaiacol","Nonnal","Hexyl acetate","Blank3","Ethyl acetate 10^8",
+                "Trans-2-hexenal 10^7", "2-butanone 10^6", "Methyl salicylate 10^6")
 names(odours.long) = odours
 
 # Get matrix
-ephys.lhns.m = physplit.analysis::createSummarySpikesMat(ephys.lhns,NALimit=3,numSamplePoints=7)
-choose.just.max1.cols = as.logical(rep(c(0,0,1,0,0,0,0),ncol(ephys.lhns.m)/7))
-ephys.lhns.m = ephys.lhns.m[,choose.just.max1.cols]
-colnames(ephys.lhns.m) = odours[1:ncol(ephys.lhns.m)]
-row.cells = dye.fills[gsub("nm20","",rownames(ephys.lhns.m))]
-rownames(ephys.lhns.m) = row.cells[,"cell.type"]
-ephys.lhns.m = ephys.lhns.m[order(rownames(ephys.lhns.m)),order(colnames(ephys.lhns.m))]
+# ephys.lh.a = physplit.analysis::createSummarySpikesArray(summary_array = ephys.lh,NALimit=3,numSamplePoints=7)
+ephys.lh.a = ephys.lh
+odours = rownames(ephys.lh.a[1,,])
+ephys.lh.m =  t(base::apply(ephys.lh.a, 1, base::t)) # Sampled at 7 time points
+choose.just.max1.cols = as.logical(rep(c(0,0,1,0,0,0,0),ncol(ephys.lh.m)/7))
+ephys.lh.m = ephys.lh.m[,choose.just.max1.cols]
+colnames(ephys.lh.m) = odours
+row.cells = rownames(ephys.lh.m)
+row.cells[row.cells%in%pns] = gloms[row.cells[row.cells%in%pns]]
+row.cells[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)] = dye.fills[gsub("nm20","",rownames(ephys.lh.m)[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)])][,"cell.type"]
+rownames(ephys.lh.m) = row.cells
+ephys.lh.m = ephys.lh.m[order(rownames(ephys.lh.m)),order(colnames(ephys.lh.m))]
 
 # Select certain odours to show
-ephys.lhns.m = ephys.lhns.m[,!colnames(ephys.lhns.m)%in%c("ClrBL","ClrB2","FlyFM")]
-colnames(ephys.lhns.m) = odours.long[colnames(ephys.lhns.m)]
+ephys.lh.m = ephys.lh.m[!rownames(ephys.lh.m)%in%c("105 PN","103 PN"),]
+ephys.lh.m = ephys.lh.m[,!colnames(ephys.lh.m)%in%c("ClrBL","ClrB2","FlyFM","ClrB1")]
+colnames(ephys.lh.m) = odours.long[colnames(ephys.lh.m)]
 
 ########
 # Save #
 ########
 
-lhn_odour_responses = ephys.lhns.m
+lhn_odour_responses = ephys.lh.m
 devtools::use_data(lhn_odour_responses,overwrite=TRUE)
 
