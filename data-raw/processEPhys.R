514c339767d63fb5960c0fe76200e69aa8e1683f
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-12-18 17:05:46 +0000

    Updated odour response matrix
    * In response to Greg’s comments: “I would like one spreadsheet with a
    simplified response for all neurons. The most canonical one is the
    window that we we used in Fig 3 0.7-2.2 s. We will also distribute the
    individual spiking responses within the R package and that will provide
    the time resolved information. @asbates if you are using the window
    0.5-1s then you are probably missing some responses.”

--- data-raw/processEPhys.R
+++ data-raw/processEPhys.R
@@ -21,7 +21,7 @@ gloms = paste0(subset(physdb, Group%in%c("PN"))[,"Anatomy.type"]," PN")
 names(gloms) = pns
 cells = c(cells,pns)
 
-# Get ephys data
+# # Get ephys data
 ephys.lh = physplit.analysis::create_raw_summary_array(x = physplitdata::smSpikes, cells = cells)
 
 # Get odours
@@ -37,29 +37,44 @@ odours.long = c("Mineral Oil", "trans-2-Hexenal", "Geranyl acetate", "Propyl ace
                 "Trans-2-hexenal 10^7", "2-butanone 10^6", "Methyl salicylate 10^6")
 names(odours.long) = odours
 
-# Get matrix
-# ephys.lh.a = physplit.analysis::createSummarySpikesArray(summary_array = ephys.lh,NALimit=3,numSamplePoints=7)
-ephys.lh.a = ephys.lh
-odours = rownames(ephys.lh.a[1,,])
-ephys.lh.m =  t(base::apply(ephys.lh.a, 1, base::t)) # Sampled at 7 time points
-choose.just.max1.cols = as.logical(rep(c(0,0,1,0,0,0,0),ncol(ephys.lh.m)/7))
-ephys.lh.m = ephys.lh.m[,choose.just.max1.cols]
-colnames(ephys.lh.m) = odours
-row.cells = rownames(ephys.lh.m)
-row.cells[row.cells%in%pns] = gloms[row.cells[row.cells%in%pns]]
-row.cells[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)] = dye.fills[gsub("nm20","",rownames(ephys.lh.m)[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)])][,"cell.type"]
-rownames(ephys.lh.m) = row.cells
-ephys.lh.m = ephys.lh.m[order(rownames(ephys.lh.m)),order(colnames(ephys.lh.m))]
+# # Get matrix
+# # ephys.lh.a = physplit.analysis::createSummarySpikesArray(summary_array = ephys.lh,NALimit=3,numSamplePoints=7)
+# ephys.lh.a = ephys.lh
+# odours = rownames(ephys.lh.a[1,,])
+# ephys.lh.m =  t(base::apply(ephys.lh.a, 1, base::t)) # Sampled at 7 time points
+# choose.just.max1.cols = as.logical(rep(c(0,0,1,0,0,0,0),ncol(ephys.lh.m)/7))
+# ephys.lh.m = ephys.lh.m[,choose.just.max1.cols]
+# colnames(ephys.lh.m) = odours
+# row.cells = rownames(ephys.lh.m)
+# row.cells[row.cells%in%pns] = gloms[row.cells[row.cells%in%pns]]
+# row.cells[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)] = dye.fills[gsub("nm20","",rownames(ephys.lh.m)[gsub("nm20","",rownames(ephys.lh.m))%in%names(dye.fills)])][,"cell.type"]
+# rownames(ephys.lh.m) = row.cells
+# ephys.lh.m = ephys.lh.m[order(rownames(ephys.lh.m)),order(colnames(ephys.lh.m))]
+#
+# # Select certain odours to show
+# ephys.lh.m = ephys.lh.m[!rownames(ephys.lh.m)%in%c("105 PN","103 PN"),]
+# ephys.lh.m = ephys.lh.m[,!colnames(ephys.lh.m)%in%c("ClrBL","ClrB2","FlyFM","ClrB1")]
+# colnames(ephys.lh.m) = odours.long[colnames(ephys.lh.m)]
 
-# Select certain odours to show
-ephys.lh.m = ephys.lh.m[!rownames(ephys.lh.m)%in%c("105 PN","103 PN"),]
-ephys.lh.m = ephys.lh.m[,!colnames(ephys.lh.m)%in%c("ClrBL","ClrB2","FlyFM","ClrB1")]
-colnames(ephys.lh.m) = odours.long[colnames(ephys.lh.m)]
+# make odour response matrix, summing smoothed spikes in the window 0.7-2.2 seconds
+sps = physplitdata::smSpikes[names(physplitdata::smSpikes)%in%cells]
+row.cells = names(sps)
+M = matrix(ncol = length(odours), nrow = length(row.cells), NA,dimnames = list(row.cells,odours))
+for(i in 1:length(sps)){
+  sp = sps[[i]]
+  for(o in odours){
+    M[i,o] = sum(sp[[o]]$counts[sp[[o]]$mids>=0.7&sp[[o]]$mids<=2.2])
+  }
+}
+colnames(M) = odours.long[colnames(M)]
+row.cells[row.cells%in%pns] = gloms[row.cells[row.cells%in%pns]]
+row.cells[gsub("nm20","",row.cells)%in%names(dye.fills)] = dye.fills[gsub("nm20","",row.cells[gsub("nm20","",row.cells)%in%names(dye.fills)])][,"cell.type"]
+M = M[!rownames(M)%in%c("105 PN","103 PN"),]
 
 ########
 # Save #
 ########
 
-lhn_odour_responses = ephys.lh.m
+lhn_odour_responses = M
 devtools::use_data(lhn_odour_responses,overwrite=TRUE)
 
