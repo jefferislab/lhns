fcf91bf586458fdcf427fc031ee6c123d04d27b2
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-04-14 11:13:07 +0100

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -1,23 +1,3 @@
-#' See a FlyCircuit neuron on the FlyCircuit website
-#'
-#' @description See chosen FlyCircuit neuron on the FlyCircuit website, where maximal projection .lsm images are available.
-#' @param fc_id A FlyCircuit neuron ID, either the neuron ID or the gene name ID
-#' @param max Maximum number of webpages to open
-#' @source \url{http://www.flycircuit.tw/}
-#' @export
-#' @importFrom utils browseURL
-#' @importFrom flycircuit fc_neuron
-see_fc <- function (fc_id, max = 10){
-  if(length(fc_id)>10){
-    stop("Too many FlyCircuit IDs!!!")
-  }
-  for(f in 1:length(fc_id)){
-    f= flycircuit::fc_neuron(fc_id)
-    url = paste0("http://flycircuit.tw/modules.php?name=clearpage&op=detail_table&neuron=",f)
-    utils::browseURL(url)
-  }
-}
-
 #' Capitalise cell type names
 #'
 #' @description Capitalise the first two letters of the cell type names
