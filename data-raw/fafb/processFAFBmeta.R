c10c5a224920f2ca2b9f0aea890ad29669c305ca
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-06-02 23:26:41 +0100

    Updates to FAFB cell typing

--- data-raw/fafb/processFAFBmeta.R
+++ data-raw/fafb/processFAFBmeta.R
@@ -7,7 +7,7 @@ library(catnat)
 library(stringi)
 
 # Read our published skids
-em.lh.meta.orig = read.csv("data-raw/csv/em_papers_lh_cell_types.csv")
+em.lh.meta.orig = read.csv("inprogress.csv") # data-raw/csv/em_papers_lh_cell_types.csv
 em.lh.meta.orig$skid = hemibrainr:::correct_id(em.lh.meta.orig$skid)
 em.lh.meta = change_nonascii(em.lh.meta.orig)
 skids = as.character(unique(em.lh.meta$skid))
@@ -21,9 +21,9 @@ matched = hemibrain_matches(priority = "FAFB")
 matched.chosen = matched[skids,]
 
 # Add new ct column
-em.lh.meta$new.cell.type = matched[as.character(em.lh.meta$skid),"cell.type"]
-em.lh.meta$new.match = matched[as.character(em.lh.meta$skid),"match"]
-em.lh.meta$is.lhn = is.lhn(em.lh.meta$new.match)
+em.lh.meta$cell_type = matched[as.character(em.lh.meta$skid),"cell.type"]
+em.lh.meta$hemibrain_match = matched[as.character(em.lh.meta$skid),"match"]
+em.lh.meta$cell = paste0(em.lh.meta$cell_type,"#",ave(em.lh.meta$cell_type,em.lh.meta$cell_type,FUN= seq.int))
 
 # Save
 fafb_lhns = em.lh.meta
